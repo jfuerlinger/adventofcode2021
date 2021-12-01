b4613e2d213f2ca3afe8fbfe956ff9b50f33bb58
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2021-12-01 20:14:41 +0100

    adds part 2 for day

--- day-01/app.js
+++ day-01/app.js
@@ -6,17 +6,35 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
     return;
   }
 
-  const numbers = data
+  const measurements = data
     .split('\n')
     .filter(str => !isNaN(str))
     .map(x => parseInt(x));
 
+
+
+  console.log(`Number of depth measurement increases (simple): ${countIncreases(measurements)}`);
+  console.log(`Number of depth measurement increases (sliding window): ${countIncreasesSlidingWindow(measurements)}`);
+});
+
+countIncreases = (measurements) => {
   let nrOfIncreases = 0;
-  for (let i = 1; i < numbers.length; i++) {
-    if (numbers[i - 1] < numbers[i]) {
+  for (let i = 1; i < measurements.length; i++) {
+    if (measurements[i - 1] < measurements[i]) {
       nrOfIncreases++;
     }
   }
 
-  console.log(`Number of depth measurement increases: ${nrOfIncreases}`);
-})
+  return nrOfIncreases;
+}
+
+countIncreasesSlidingWindow = (measurements) => {
+  let nrOfIncreases = 0;
+  const windowSums = [];
+
+  for (let i = 1; i < measurements.length-1; i++) {
+    windowSums.push(measurements[i-1] + measurements[i] + measurements[i+1])
+  }
+
+  return countIncreases(windowSums);
+}
