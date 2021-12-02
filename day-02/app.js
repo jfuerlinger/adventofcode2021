c6c496938328e42e8fd2bfb16e681ed158a31498
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2021-12-02 12:57:09 +0100

    add part 2 for day 2

--- day-02/app.js
+++ day-02/app.js
@@ -9,11 +9,11 @@ fs.readFile('./input.txt', 'utf8', (err, data) => {
   const movements = data
     .split('\n');
 
-  console.log(`puzzle's solution is ${processMovements(movements)}`);
-
+  console.log(`puzzle's solution (part1) is ${processMovementsPart1(movements)}`);
+  console.log(`puzzle's solution (part2) is ${processMovementsPart2(movements)}`);
 });
 
-processMovements = (movements) => {
+processMovementsPart1 = (movements) => {
   let posHorizontal = 0;
   let posDepth = 0;
   for (let i = 0; i < movements.length; i++) {
@@ -32,6 +32,34 @@ processMovements = (movements) => {
 
       case 'up':
         posDepth -= amount;
+        break;
+    }
+  }
+
+  return posHorizontal * posDepth;
+}
+
+processMovementsPart2 = (movements) => {
+  let posHorizontal = 0;
+  let posDepth = 0;
+  let aim = 0;
+  for (let i = 0; i < movements.length; i++) {
+    let movement = movements[i].split(' ');
+    let direction = movement[0];
+    let amount = parseInt(movement[1]);
+
+    switch (direction) {
+      case 'forward':
+        posHorizontal += amount;
+        posDepth += aim * amount;
+        break;
+
+      case 'down':
+        aim += amount;
+        break;
+
+      case 'up':
+        aim -= amount;
         break;
     }
   }
