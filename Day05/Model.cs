8a8f3832f53f34a5ad67d75a41fab5dda5a1b844
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2021-12-24 10:49:47 +0100

    add day-05

---
+++ Day05/Model.cs
@@ -0,0 +1,103 @@
+﻿using System.Text;
+
+namespace Day05
+{
+    internal record Position(int X, int Y);
+
+    internal record NavigationLine(Position Start, Position End);
+
+    internal class Field
+    {
+        private readonly ushort[,] _field;
+
+        public Field(int fieldDimension)
+        {
+            _field = new ushort[fieldDimension, fieldDimension];
+        }
+
+        public ushort this[int row, int column]
+        {
+            get => _field[row, column];
+            private set => _field[row, column] = value;
+        }
+
+
+        public ushort this[Position position]
+        {
+            get => _field[position.Y, position.X];
+            private set => _field[position.Y, position.X] = value;
+        }
+
+        public void AddNavigationLine(NavigationLine line)
+        {
+            if (line.Start == line.End)
+            {
+                throw new InvalidOperationException("Start and End are at some Position");
+            }
+
+            Position currentPosition = line.Start;
+            this[currentPosition]++;
+
+            while (currentPosition != line.End)
+            {
+                currentPosition = GetNextPositionTowardsTarget(currentPosition, line.End);
+                this[currentPosition]++;
+            }
+        }
+
+        public int GetDangerousPositions()
+        {
+            int dangerCount = 0;
+            for (int row = 0; row < _field.GetLength(0); row++)
+            {
+                for (int column = 0; column < _field.GetLength(1); column++)
+                {
+                    if(this[row, column] >= 2)
+                    {
+                        dangerCount++;
+                    }
+                }
+            }
+
+            return dangerCount;
+        }
+
+        private static Position GetNextPositionTowardsTarget(Position current, Position target)
+        {
+            if (current == null)
+            {
+                throw new ArgumentNullException(nameof(current));
+            }
+
+            return new Position(
+                GetDelta(current.X, target.X),
+                GetDelta(current.Y, target.Y));
+        }
+
+        private static int GetDelta(int current, int target)
+        {
+            if (current == target)
+            {
+                return current;
+            }
+
+            return current < target ? current + 1 : current - 1;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int row = 0; row < _field.GetLength(0); row++)
+            {
+                for (int column = 0; column < _field.GetLength(1); column++)
+                {
+                    sb.Append(this[row, column] == 0 ? "." : this[row, column]);
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ Day05/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using Day05;
+using Figgle;
+
+Console.WriteLine(FiggleFonts.Big.Render("AoC 2021 - Day 5"));
+
+string[] lines = await File.ReadAllLinesAsync("input.txt");
+
+var navigationLines = BuildNavigationLines(lines);
+
+SolvePuzzle(
+    "part 1",
+    navigationLines
+        .Where(line => line.Start.X == line.End.X || line.Start.Y == line.End.Y));
+
+SolvePuzzle(
+    "part 2",
+    navigationLines);
+
+
+
+void SolvePuzzle(string puzzleName, IEnumerable<NavigationLine> lines)
+{
+    Field fieldPart = new(GetDimension(lines));
+    lines.ToList()
+        .ForEach(line => fieldPart.AddNavigationLine(line));
+    //Console.WriteLine(fieldPart);
+
+    Console.WriteLine($"Solution '{puzzleName}' -> fields of danger: {fieldPart.GetDangerousPositions()}");
+}
+
+
+int GetDimension(IEnumerable<NavigationLine> lines) =>
+    new int[] {
+            lines.Max(line => line.Start.X),
+            lines.Max(line => line.Start.Y),
+            lines.Max(line => line.End.X),
+            lines.Max(line => line.End.Y)
+    }.Max() + 1;
+
+IEnumerable<NavigationLine> BuildNavigationLines(string[] lines) =>
+    lines.Select(line =>
+    {
+        var points = line.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+        var point1Information = points[0].Split(",");
+        var point2Information = points[1].Split(",");
+
+        return new NavigationLine(
+            new Position(ushort.Parse(point1Information[0]), ushort.Parse(point1Information[1])),
+            new Position(ushort.Parse(point2Information[0]), ushort.Parse(point2Information[1])));
+    });
+
