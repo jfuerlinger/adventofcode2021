3c015c0e3e4b46679140dc84d5dac83d0b9dddf4
Josef Fürlinger <josef.fuerlinger@smartpoint.at>
2021-12-25 21:49:13 +0100

    add day 08 part 01

---
+++ Day08/Model.cs
@@ -0,0 +1,16 @@
+﻿namespace Day08
+{
+    internal record NoteEntry(string[] SignalPatterns, string[] OutputValues);
+    
+    //internal class NoteEntry
+    //{
+    //    public string[] SignalPatterns { get; init; }
+    //    public string[] OutputValues { get; init; }
+
+    //    public NoteEntry(string[] signalPatterns, string[] outputValues)
+    //    {
+    //        SignalPatterns = signalPatterns;
+    //        OutputValues = outputValues;
+    //    }
+    //}
+}
---
+++ Day08/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using Day08;
+using Figgle;
+
+Console.WriteLine(FiggleFonts.Big.Render("AoC 2021 - Day 8"));
+
+string[] lines = await File.ReadAllLinesAsync("input.txt");
+
+SolvePuzzle1(lines);
+
+static void SolvePuzzle1(string[] lines)
+{
+    var notes = lines
+           .Select(line => line.Split("|", StringSplitOptions.RemoveEmptyEntries))
+           .Select(parts => new NoteEntry(
+               parts[0].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries),
+               parts[1].Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+           ))
+           .ToArray();
+
+    int solutionPart1 = notes
+        .SelectMany(entry => entry.OutputValues)
+        .Count(entry => new[] { 2 /* -> 1 */,
+                                4 /* -> 4 */,
+                                3 /* -> 7 */,
+                                7 /* -> 8 */}.Contains(entry.Length));
+
+    Console.WriteLine($"Solution part 1: {solutionPart1}");
+}
+
+
+
+
